b41502ba11366288ad7528c0d1c306e850a72335
ArviRizqi <85241896+ArviRizqi@users.noreply.github.com>
2025-06-12 09:43:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
+from huggingface_hub import hf_hub_download
 import streamlit as st
 import torch
 from torchvision.models import resnet50, ResNet50_Weights
 from torchvision import transforms
 from PIL import Image
 import os
-import gdown
 
 st.set_page_config(page_title="Klasifikasi Batik", layout="centered")
 st.title("🧵 Klasifikasi Batik Indonesia")
-st.write("Unggah gambar batik dan lihat jenisnya berdasarkan model klasifikasi PyTorch.")
 
-# Konstanta
-MODEL_ID = "1TcwfUAOUvVCYXwfvKHc9F1UvEEMuaxw2"
-MODEL_PATH = "batik_model.pth"
+# Setup
+REPO_ID = "Artz-03/batik-model"  # Ganti dengan repo kamu
+FILENAME = "batik_model.pth"
 
-# Download model jika belum ada
-if not os.path.exists(MODEL_PATH):
-    with st.spinner("Mengunduh model..."):
-        gdown.download(id=MODEL_ID, output=MODEL_PATH, quiet=False)
+# Download model dari Hugging Face Hub (hanya pertama kali)
+MODEL_PATH = hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
 
 # Load model
 @st.cache_resource
@@ -32,7 +29,6 @@ def load_model():
 
 model, transform = load_model()
 
-# Label
 CLASS_NAMES = [
     'batik-bali', 'batik-lasem', 'batik-betawi', 'batik-megamendung',
     'batik-pekalongan', 'batik-parang', 'batik-kawung', 'batik-ciamis',
@@ -41,16 +37,13 @@ CLASS_NAMES = [
     'batik-garutan', 'batik-sidoluhur', 'batik-sekar', 'batik-sogan'
 ]
 
-# Upload
 uploaded_file = st.file_uploader("Unggah gambar batik", type=["jpg", "png", "jpeg"])
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Gambar yang diunggah", use_container_width=True)
-
     img_tensor = transform(image).unsqueeze(0)
     with torch.no_grad():
         output = model(img_tensor)
         _, predicted = torch.max(output, 1)
-        class_idx = predicted.item()
-        prediction = CLASS_NAMES[class_idx]
+        prediction = CLASS_NAMES[predicted.item()]
         st.success(f"✅ Prediksi: **{prediction}**")
